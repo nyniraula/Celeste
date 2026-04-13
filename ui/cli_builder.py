beb5e9c6b245b0acd1cb64026dd6f0852a0b851d
nyniraula <nyniraula@gmail.com>
2026-04-13 17:05:10 +0545

    Expand CLIBUILDER class

--- ui/cli_builder.py
+++ ui/cli_builder.py
@@ -1,10 +1,20 @@
 import questionary
+from pyfiglet import figlet_format
 from rich.console import Console
+from rich.padding import Padding
 
 
 class CLIBuilder:
     def __init__(self):
+        self.title = "celeste"
+        self.desc = "Heavenly downloads, no divine intervention required."
         self.console = Console()
+
+    def cli_banner(self):
+        banner = figlet_format(self.title, font="sblood")
+        # fonts: banner3-D , banner4, poison, larry3d,contrast, cosmic, roman, rowancap, sblood
+        self.console.print(Padding(f"[#173024]{banner}[/]", (0, 2)))
+        self.console.print(Padding(f"[dim]{self.desc}", (0, 2)))
 
     def url_input(self, fn_validate):
         url = questionary.text(
