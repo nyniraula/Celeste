c921216656511efefc8ab9c65d364cfb198cd3b4
nyniraula <nyniraula@gmail.com>
2026-04-13 10:55:01 +0545

    ADD: FFMPEG & JS RUNTIME CHECK to DEP RESOLVER

--- core/dependency_resolver.py
+++ core/dependency_resolver.py
@@ -3,10 +3,20 @@ import shutil
 
 class DependencyResolver:
     def _check_existence(self, tool):
-        """Return the path if the tool exists, if not returns false"""
+        """Returns the path if the tool exists, if not returns false"""
         path = shutil.which(tool)
         return path if bool(path) else False
 
+    def has_ffmpeg(self):
+        return self._check_existence("ffmpeg")
 
-dr = DependencyResolver()
-dr._check_existence("ffmpeg")
+    def has_js_runtime(self):
+        deno_path = self._check_existence("deno")
+        node_path = self._check_existence("node")
+
+        if deno_path:
+            return deno_path
+        elif node_path:
+            return node_path
+        else:
+            return None
