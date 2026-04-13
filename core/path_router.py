1d6211df67a60ee52e910a12db637336c07a4e0a
nyniraula <nyniraula@gmail.com>
2026-04-13 16:30:37 +0545

    Reinvent dwnld path determination logic

--- core/path_router.py
+++ core/path_router.py
@@ -9,15 +9,10 @@ class PathRouter:
 
     def resolve_output_path(self):
 
-        path_segments = self.output_template.split(
-            "/"
-        )  # splits the outtmpl into array by /
-        path_segments.pop()  # removes  the %(title)s - %(uploader)s_[%(height)sp].%(ext)s part
-
-        relative_path = "/".join(path_segments)
-
-        download_path = Path.home() / relative_path
+        download_path = (
+            Path.home() / Path(self.output_template).parent
+        )  # gets the parent directory of the outtmpl
 
         # Creates a new dir (both parent and children) if any missing according to the path, leaves it as it is if found (a safer way to not overwrite preexisting dir)
         download_path.mkdir(exist_ok=True, parents=True)
-        print(download_path)
+        # print(download_path)
